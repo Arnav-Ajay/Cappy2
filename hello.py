fc5b90a1881a6d59644d3a1247abaf85fe9ff27f
Arnav-Ajay <arnavaj@gmail.com>
2019-09-28 16:50:31 +0530

    final commit

--- hello.py
+++ hello.py
@@ -3,17 +3,23 @@ import diff
 import mult
 import division
 
-g = raw_input("Enter 1 for sum \n Enter 2 for difference \n Enter 3 for product \n Enter 4 for division \n Enter 0 to exit")
+#print("Enter 1 for sum \n Enter 2 for difference \n Enter 3 for product \n Enter 4 for division \n Enter 0 to exit")
 
-if g == 1:
+g = int(input("Enter 1 for sum \n Enter 2 for difference \n Enter 3 for product \n Enter 4 for division \n Enter 0 to exit"))
+
+if g == 1 :
 	c = add.sum(5, 6) 
-elif g == 2
+	print(c)
+elif g == 2 :
 	c = diff.diff(6, 5)
-elif g ==3
-	mult.mult(5, 2)
-elif g==4
-	division.division_numbers(10, 2)
-else 
+	print(c)
+elif g ==3 :
+	c = mult.mult(5, 2)
+	print(c)
+elif g==4 :
+	c = division.division_numbers(10, 2)
+	print(c)
+else:
 	print("exiting")
 	exit(0)
 
