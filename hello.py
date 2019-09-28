5c59095d2b5f2846ee39d3ddc2634de05ec31a2c
Klaus-poppe <vesparadkt@gmail.com>
2019-09-28 16:16:41 +0530

    hello.py

---
+++ hello.py
@@ -0,0 +1,22 @@
+g = raw_input("Enter 1 for sum \n Enter 2 for difference \n Enter 3 for product \n Enter 4 for division \n Enter 0 to exit")
+
+def sum():
+
+def diff():
+
+def product():
+
+def division():
+
+if g == 1:
+	sum() 
+elif g == 2
+	diff()
+elif g ==3
+	product()
+elif g==4
+	division()
+else 
+	print("exiting")
+	exit(0)
+
